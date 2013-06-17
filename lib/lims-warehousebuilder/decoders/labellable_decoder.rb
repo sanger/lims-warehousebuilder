5ddebe9d716a0a935dae719dbaff22a8c8ff4fb2
Loic <llh1@sanger.ac.uk>
2013-06-17 14:45:10 +0100

    Error management in labellable decoder

--- lib/lims-warehousebuilder/decoders/labellable_decoder.rb
+++ lib/lims-warehousebuilder/decoders/labellable_decoder.rb
@@ -1,6 +1,4 @@
 require 'lims-warehousebuilder/json_decoder'
-require 'rubygems'
-require 'ruby-debug/debugger'
 
 module Lims::WarehouseBuilder
   module Decoder
@@ -19,14 +17,19 @@ module Lims::WarehouseBuilder
         [].tap do |barcodes|
           @payload["labels"].each do |position, label|
             label_value = label["value"]
-            barcode = case label["type"]
-                      when /sanger/ then klass.barcode_by_sanger_barcode(label_value) 
-                      when /ean13/ then klass.barcode_by_ean13_barcode(label_value)
-                      end
+            begin
+              barcode = case label["type"]
+                        when /sanger/ then klass.barcode_by_sanger_barcode(label_value) 
+                        when /ean13/ then klass.barcode_by_ean13_barcode(label_value)
+                        end
 
-            barcode.barcoded_resource_uuid = barcoded_resource_uuid
-            barcode.position = position
-            barcodes << barcode
+              new_barcode = clone_model_object(barcode)
+              new_barcode.barcoded_resource_uuid = barcoded_resource_uuid
+              new_barcode.position = position
+              barcodes << new_barcode
+            rescue Model::NotFound => e
+              raise MessageToBeRequeued.new(e.message)
+            end
           end
         end
       end
