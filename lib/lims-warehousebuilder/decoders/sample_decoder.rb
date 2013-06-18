2916dd770b4f82ffa169bb2ed3d361a58ac60e3b
Loic <llh1@sanger.ac.uk>
2013-06-18 16:14:16 +0100

    Save in Sample Container Helper the container model

--- lib/lims-warehousebuilder/decoders/sample_decoder.rb
+++ lib/lims-warehousebuilder/decoders/sample_decoder.rb
@@ -13,7 +13,6 @@ module Lims::WarehouseBuilder
           if @payload["ancestor_uuid"]
             # Sample which are part from other laboratory asset like tubes...
             objects << sample_container_association
-            objects << sample_management_activity
           else
             # Sample object from lims-management-app. Are not part
             # of other s2 resource so they don't have an ancestor_uuid.
@@ -33,17 +32,9 @@ module Lims::WarehouseBuilder
       def sample_container_association
         Model::SampleContainerHelper.new({
           :sample_uuid => @payload["uuid"],
-          :container_uuid => @payload["ancestor_uuid"]
+          :container_uuid => @payload["ancestor_uuid"],
+          :container_model => @payload["ancestor_type"]
         })
-      end
-
-      # @return [Sequel::Model]
-      # Initialize the row in the table sample_management_activity
-      # for the sample, if it doesn't exist yet.
-      def sample_management_activity
-        sample_uuid = @payload["uuid"]
-        klass = model_for("sample_management_activity")
-        klass.activity_for_sample?(sample_uuid) ? nil : klass.new(:uuid => sample_uuid)
       end
     end
   end
