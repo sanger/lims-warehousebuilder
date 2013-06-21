873ba5fb1c45c4fb00130f03c83563a104ba677d
Loic <llh1@sanger.ac.uk>
2013-06-21 11:58:12 +0100

    Item model

--- lib/lims-warehousebuilder/models/item.rb
+++ lib/lims-warehousebuilder/models/item.rb
@@ -5,9 +5,31 @@ module Lims::WarehouseBuilder
     class Item < Sequel::Model(:historic_items)
 
       include ResourceTools::Mapping
+      # TODO: trigger in before_save to update items current table
+      # Handle only one row per item uuid. Need to be fixed if a resource
+      # appears in more than one order.
       include Common
 
-      translate(:batch__uuid => :batch_uuid)
+      def before_create
+        set_order_id!
+      end
+
+      def set_order_uuid(order_uuid)
+        @order_uuid = order_uuid
+      end
+
+      def self.item_by_uuid(item_uuid)
+        item = self.dataset.from(current_table_name).where(:uuid => item_uuid).first
+        raise NotFound, "The item corresponding to the resource #{item_uuid} cannot be found" unless item
+        item
+      end
+
+      private
+
+      def set_order_id!
+        order = Model.model_for_uuid(@order_uuid, "order")
+        self.order_id = order.internal_id
+      end
     end
   end
 end
