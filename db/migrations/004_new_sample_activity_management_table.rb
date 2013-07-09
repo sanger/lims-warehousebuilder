56e0da4fdc6c9250065719c0f792bde34a4f2042
Loic <llh1@sanger.ac.uk>
2013-07-09 11:07:15 +0100

    Use String column to store uuid

--- db/migrations/004_new_sample_activity_management_table.rb
+++ db/migrations/004_new_sample_activity_management_table.rb
@@ -2,12 +2,12 @@ Sequel.migration do
   change do
     create_table :sample_management_activity do
       primary_key :internal_id
-      foreign_key :uuid, :current_samples, :key => :uuid 
-      foreign_key :order_uuid, :current_orders, :key => :uuid 
+      String :uuid, :fixed => true, :size => 64 
+      String :order_uuid, :fixed => true, :size => 64 
       String :process
       String :step
-      foreign_key :tube_uuid, :current_tubes, :key => :uuid
-      foreign_key :spin_column_uuid, :current_spin_columns, :key => :uuid
+      String :tube_uuid, :fixed => true, :size => 64 
+      String :spin_column_uuid, :fixed => true, :size => 64 
       String :user
       String :status
       DateTime :date
--- db/migrations/007_add_rack_info_in_tubes_tables.rb
+++ db/migrations/007_add_rack_info_in_tubes_tables.rb
@@ -7,8 +7,6 @@ Sequel.migration do
     end
 
     alter_table :historic_tubes do
-      # The following foreign key doesn't work...
-      #add_foreign_key :tube_rack_uuid, :historic_tube_racks, :key => :uuid
       add_column :tube_rack_uuid, String, :fixed => true, :size => 64
       add_column :location, String
     end
