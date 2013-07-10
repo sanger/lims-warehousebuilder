a661139d1089acc8d93d527c8fab84efd68be74b
Loic <llh1@sanger.ac.uk>
2013-07-10 14:20:32 +0100

    Refactor labellable decoder to set the ean13 position and sanger position on the same barcode record

--- lib/lims-warehousebuilder/decoders/labellable_decoder.rb
+++ lib/lims-warehousebuilder/decoders/labellable_decoder.rb
@@ -12,24 +12,38 @@ module Lims::WarehouseBuilder
 
       def barcodes
         barcoded_resource_uuid = @payload["name"]
+        date = @payload["date"]
+        user = @payload["user"]
         klass = Model.model_for("barcode")
+        tmp_barcodes = {}
+
+        @payload["labels"].each do |position, label|
+          label_value = label["value"]
+          label_type = case label["type"]
+                       when /sanger/ then "sanger"
+                       when /ean13/ then "ean13"
+                       end
+
+          begin
+            barcode = klass.send("barcode_by_#{label_type}_barcode", label_value)
+          rescue Model::NotFound => e
+            raise MessageToBeRequeued.new(e.message)
+          end
+
+          tmp_barcodes[barcode] ||= []
+          tmp_barcodes[barcode] << {:type => label_type, :position => position}
+        end
 
         [].tap do |barcodes|
-          @payload["labels"].each do |position, label|
-            label_value = label["value"]
-            begin
-              barcode = case label["type"]
-                        when /sanger/ then klass.barcode_by_sanger_barcode(label_value) 
-                        when /ean13/ then klass.barcode_by_ean13_barcode(label_value)
-                        end
-
-              new_barcode = Model.clone_model_object(barcode)
+          tmp_barcodes.each do |barcode, labels|
+            new_barcode = Model.clone_model_object(barcode)
+            labels.each do |label|
               new_barcode.barcoded_resource_uuid = barcoded_resource_uuid
-              new_barcode.position = position
-              barcodes << new_barcode
-            rescue Model::NotFound => e
-              raise MessageToBeRequeued.new(e.message)
+              new_barcode.send("#{label[:type]}_barcode_position=", label[:position])
+              new_barcode.updated_at = date
+              new_barcode.updated_by = user
             end
+            barcodes << new_barcode
           end
         end
       end
