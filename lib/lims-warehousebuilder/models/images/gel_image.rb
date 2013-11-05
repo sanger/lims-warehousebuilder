dcadc81b451d4d782cac08ab646a01bfb2c44788
Loic <llh1@sanger.ac.uk>
2013-11-05 17:07:38 +0000

    Remove unused code

--- lib/lims-warehousebuilder/models/images/gel_image.rb
+++ lib/lims-warehousebuilder/models/images/gel_image.rb
@@ -6,12 +6,6 @@ module Lims::WarehouseBuilder
       include Common
 
       one_to_one :gel_image_metadata
-
-      # @param [String] uuid
-      # @return [Model::GelImage]
-      def self.gel_image_by_uuid(uuid)
-        Model.model_by_uuid(uuid, :gel_image) 
-      end
     end
   end
 end
