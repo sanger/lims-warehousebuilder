cc6e6bd8cfd746e2d32e84849d0249363a72faac
Loic <llh1@sanger.ac.uk>
2013-11-05 17:06:51 +0000

    Remove uuid from gel_images and put it in gel_image_metadata with an index

--- db/migrations_images/001_create_tables.rb
+++ db/migrations_images/001_create_tables.rb
@@ -2,15 +2,16 @@ Sequel.migration do
   up do
     create_table :gel_images do
       primary_key :internal_id
-      String :uuid, :fixed => true, :size => 64
       Blob :image
     end
 
     create_table :gel_image_metadata do
       primary_key :internal_id
       foreign_key :gel_image_id, :gel_images, :key => :internal_id 
+      String :uuid, :fixed => true, :size => 64
       String :score
       DateTime :created_at
+      index :uuid
     end
   end
 
