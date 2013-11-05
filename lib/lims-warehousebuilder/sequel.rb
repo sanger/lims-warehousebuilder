91d8c4c95436b9e264fb5915caa483186a1d1d42
Loic <llh1@sanger.ac.uk>
2013-11-05 15:02:09 +0000

    Connect to warehouse and warehouse_images, creating two datasets

--- lib/lims-warehousebuilder/sequel.rb
+++ lib/lims-warehousebuilder/sequel.rb
@@ -1,5 +1,8 @@
-unless defined?(DB)
-  DB = Sequel.connect(YAML.load_file(File.join('config', 'database.yml'))[ENV["LIMS_WAREHOUSEBUILDER_ENV"]])
+unless defined?(DB) && defined?(DB_IMAGES)
+  env = ENV["LIMS_WAREHOUSEBUILDER_ENV"]
+  db_settings = YAML.load_file(File.join('config', 'database.yml'))
+  DB = Sequel.connect(db_settings["warehouse_#{env}"])
+  DB_IMAGES = Sequel.connect(db_settings["warehouse_images_#{env}"])
 end
 
 Sequel.default_timezone = :utc
