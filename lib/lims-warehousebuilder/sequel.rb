ce9afee055619393a8954549b5c200ed1ab16eb1
Loic <llh1@sanger.ac.uk>
2013-12-11 13:52:40 +0000

    Remove reference to gel image

--- lib/lims-warehousebuilder/sequel.rb
+++ lib/lims-warehousebuilder/sequel.rb
@@ -1,4 +1,4 @@
-unless defined?(DB) && defined?(DB_IMAGES)
+unless defined?(DB)
   env = ENV["LIMS_WAREHOUSEBUILDER_ENV"]
   db_settings = YAML.load_file(File.join('config', 'database.yml'))
   DB = Sequel.connect(db_settings[env])
